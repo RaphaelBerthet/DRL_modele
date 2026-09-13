654e31f4d2f890d69d15c997c90a7cf3ccfebcbd
RaphaelBerthet <raphael.berthet45@gmail.com>
2026-09-13 15:46:15 +0200

    renomme dossier en packages

--- entrainement_modele.py
+++ entrainement_modele.py
@@ -1,6 +1,6 @@
-from entrainement_reseau_objets_utiles.build_state import build_state
-from entrainement_reseau_objets_utiles.entrainement_reseau_neurones import Reseau_neurones
-from entrainement_reseau_objets_utiles.parametres import NB_NEURONES_LAYER1, NB_NEURONES_LAYER2, TAILLE_STATE, NB_ACTIONS_POSSIBLES, NB_PARTIES, p_debut, p_fin
+from packages.build_state import build_state
+from packages.entrainement_reseau_neurones import Reseau_neurones
+from packages.parametres import NB_NEURONES_LAYER1, NB_NEURONES_LAYER2, TAILLE_STATE, NB_ACTIONS_POSSIBLES, NB_PARTIES, p_debut, p_fin
 import random
 import numpy as np
 from numpy.typing import NDArray
